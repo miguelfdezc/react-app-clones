78c1ab2daf4b7cbc1c16622cdb678dbcd246f334
Miguel Fernández Carratalá <miguelfdezc99@gmail.com>
2020-09-11 00:31:28 +0200

    Amazon Clone (Day 2 ReactJS Challenge)

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDh780mldzodsETgchhqGgZ_84YT62hJ2Q",
+    authDomain: "challenge-dc26d.firebaseapp.com",
+    databaseURL: "https://challenge-dc26d.firebaseio.com",
+    projectId: "challenge-dc26d",
+    storageBucket: "challenge-dc26d.appspot.com",
+    messagingSenderId: "392366272374",
+    appId: "1:392366272374:web:273b970db5a32862845457",
+    measurementId: "G-116S89GVLF"
+};
